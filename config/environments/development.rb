d9e5ff3b603a6e2433ce9ee19fdb0e4172c7ed40
Brian Holt <bholtbholt@icloud.com>
2013-08-09 23:04:08 -0700

    Added users with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Kite::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Action for Devise Gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Kite::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # This is for Devise, needs to be updated with an actual host for correct depolyment
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
