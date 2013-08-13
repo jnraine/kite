4a5ccb23e79d41155027ceaac22b0b82959f762b
Brian Holt <bholtbholt@icloud.com>
2013-08-13 10:34:10 -0700

    Enable JS HTML5 GeoLocation

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,10 @@
 $('.event-div').click( function() {
   $(this).children('.event-details').toggle(200)
 });
+
+function showPosition(position) {
+  //alert('Lat: ' + position.coords.latitude + ', Lon: ' + position.coords.longitude);
+}
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
