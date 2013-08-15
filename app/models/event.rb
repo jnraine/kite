76dfc9286cd064db8f650c2d2fa7c4124368ca12
Brian Holt <bholtbholt@icloud.com>
2013-08-15 10:10:40 -0700

    Convert -bash costs to Free, Align headers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :address, :cost, :date, :details, :end_time, :fav, :start_time, :title, :venue
   belongs_to :user
-  validates :user_id, presence: true
-  validates :address, :cost, :title, :venue, presence: true
+  validates :address, :cost, :title, :user_id, :venue, presence: true
+  validates_length_of :title, :maximum => 70
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 end
