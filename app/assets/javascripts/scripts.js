df1d4c08f2d4700db7df126f2406a85729750e21
Brian Holt <bholtbholt@icloud.com>
2013-08-15 09:20:56 -0700

    Fix event toggle, event padding, and adjust home view

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,7 +2,7 @@ $('.event-div').click( function() {
 	var openEvent = $(this).children('.event-details');
 
 	$('.event-details').not(openEvent).hide(200);
-	openEvent.show(200);
+	openEvent.toggle(200);
 });
 
 // $('.form-popover').popover( { placement: 'auto' })
