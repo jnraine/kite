cc40e85a7833da5edfd2a12ea2d2d64c367b368a
Brian Holt <bholtbholt@icloud.com>
2013-11-11 18:57:49 -0800

    Code Cleanup

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,13 @@
 module EventsHelper
 	def toggle_fav(event, user)
-		link_to user.flagged?(event, :fav) ?
-			content_tag(:span, " ", :class => "glyphicon glyphicon-heart") :
-			content_tag(:span, " ", :class => "glyphicon glyphicon-heart-empty"),
-			fav_event_path(event)
-			#:remote => true
+		if user_signed_in?
+			link_to user.flagged?(event, :fav) ?
+				content_tag(:span, " ", :class => "glyphicon glyphicon-heart") :
+				content_tag(:span, " ", :class => "glyphicon glyphicon-heart-empty"),
+				fav_event_path(event)
+				#:remote => true
+		else
+			link_to content_tag(:span, " ", :class => "glyphicon glyphicon-heart-empty"), new_user_session_path
+		end
 	end
 end
--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,8 +1,12 @@
 module VenuesHelper
 	def toggle_subscribe(venue, user)
-		link_to user.flagged?(venue, :unsubscribe) ?
-			content_tag(:span, " ", :class => "glyphicon glyphicon-plus") :
-			content_tag(:span, " ", :class => "glyphicon glyphicon-remove"),
-			unsubscribe_venue_path(venue)
+		if user_signed_in?
+			link_to user.flagged?(venue, :unsubscribe) ?
+				content_tag(:span, " ", :class => "glyphicon glyphicon-plus") :
+				content_tag(:span, " ", :class => "glyphicon glyphicon-remove"),
+				unsubscribe_venue_path(venue)
+		else
+			link_to content_tag(:span, " ", :class => "glyphicon glyphicon-remove"), new_user_session_path
+		end
 	end
 end
