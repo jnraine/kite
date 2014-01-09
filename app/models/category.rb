6a2da5aff7a154b72554c9383c88193c451bd227
Brian Holt <bholtbholt@icloud.com>
2014-01-09 13:51:22 -0800

    Make Events dependent on Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   attr_protected :name
-  has_many :events
+  has_many :events, :dependent => :destroy
 
   default_scope order('name ASC')
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# fitness and training, outdoor activities, indoor activities, local attractions
+# fitness and training, outdoor activities, indoor activities, local attractions 
+# "Stand-up Comedy",
+# "Shopping Sales",
+# "Drink Specials",
+# "Food Specials",
+# "Community Events",
+# "Games and Trivia",
+# "Workshops and Classes"
 
 ["Live Music",
  "Night Clubs",
  "Theatre Performances",
- "Stand-up Comedy",
  "Sporting Events",
- "Art Shows",
- "Shopping Sales",
- "Drink Specials",
- "Food Specials",
- "Community Events",
- "Games and Trivia",
- "Workshops and Classes"
+ "Art Shows"
  ].each do |category|
     Category.find_or_create_by_name(category)
 end
