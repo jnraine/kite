576005b5336556fae2e5529c30f0142dd91c1c69
Brian Holt <bholtbholt@icloud.com>
2014-02-28 12:59:42 -0800

    Rebuild and clean up main navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 	def navigation_icon
 		if @url_location == root_path
-			link_to "here", root_path, :class => "navbar-brand logo"
+			link_to "here", root_path, :class => "lg-icon logo"
 		else
 			@url_location ||= root_path
-			link_to content_tag(:span, " ", :class => "glyphicon glyphicon-arrow-left navbar-brand"), @url_location
+			link_to content_tag(:span, " ", :class => "glyphicon glyphicon-arrow-left lg-icon"), @url_location
 		end
 	end
 	def root_path?
